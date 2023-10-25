a2ceab7999bb930f856bc90109725df9354f0ee5
khomathi1085 <130474818+khomathi1085@users.noreply.github.com>
2023-10-25 07:46:44 +0530

    Add files via upload

---
+++ src/main/java/com/example/netfixclone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.netfixclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.netflix);
+    }
+}
